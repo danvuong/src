fe6a419ce4f99053b52a684de08150db0affaf14
serge <merlin.boyer@wanadoo.fr>
2018-05-18 12:15:57 +0200

    source du cache

--- MyFileCache.cpp
+++ MyFileCache.cpp
@@ -1,2 +1,33 @@
 #include "myfilecache.h"
+#include <iostream>
 
+QHash<QString, QByteArray> MyFileCache::cache;
+
+
+
+MyFileCache::MyFileCache(QWidget *parent){
+
+}
+
+//STATIC
+void MyFileCache::UpdateCache(QString name){
+    if (!cache.contains(name)){
+        QFile *file = new QFile( name );
+        cache.insert(name, file->readAll());
+        std::cout << "###### CACHE UPDATED : " << name.toStdString() << std::endl;
+    }
+}
+
+int MyFileCache::IsInCache(QString name){
+    if (cache.contains(name)){
+        return 1;  //le fichier est deja dans le cache
+    }else{
+        return 0;   //Le fichier n'est pas dans le cache
+    }
+}
+
+
+void MyFileCache::LoadCache(QString name, QFile *file){
+    file->write(cache[name]);
+    std::cout << "###### LOADED FROM CACHE : " << name.toStdString() << std::endl;
+}
--- MyFileCache.h
+++ MyFileCache.h
@@ -1,4 +1,35 @@
 #ifndef MYFILECACHE_H
 #define MYFILECACHE_H
 
+
+#include <QtWidgets/QDialog>
+#include <QString>
+#include <string>
+#include <QHash>
+#include <QFile>
+#include <QByteArray>
+
+
+
+class MyFileCache
+{
+
+public:
+    MyFileCache(QWidget *parent = 0);
+    ~MyFileCache();
+    static void UpdateCache(QString name);
+    static void LoadCache(QString name, QFile *file);
+    static int IsInCache(QString name);
+
+
+private:
+    static int count;   //
+    static QHash<QString , QByteArray> cache;
+
+
+public slots:
+
+
+};
+
 #endif
